ff1121209317f0d4d90ff51cf3184d61f679b2b9
Ray Velasquez <rayvelasquez@Rays-MacBook-Pro.local>
2020-10-24 22:51:33 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+## USING THIS TEMPLATE:
+
+- Make sure to run npm install
+
+- rename the template.env to .env
+
+- auth.js has most of the auth middleware logic built out, just create a verify user function based on your particular database setup.
+
+- cors whitelist defined in config/cors.js, when in a non-production environment it will allow all traffic, in production it will work off the whitelist.
+
+## Commands
+
+```npm run dev``` run dev server look for env.yaml for environmental variables
+
+```npm run start``` run in production more, not look for env.yaml
+
+MAKE SURE TO JOIN THE SLACK AND DISCORD COMMUNITIES AT DEVNURSERY.COM
---
+++ server.js
@@ -0,0 +1,54 @@
+///////////////////////////
+// Environmental Variables
+///////////////////////////
+require("dotenv").config()
+const {
+  PORT = 3000,
+  SECRET = "secret",
+  NODE_ENV = "development",
+} = process.env;
+
+//CORS
+const cors = require("cors");
+const corsOptions = require("./configs/cors.js");
+
+//AUTH
+const jwt = require("jsonwebtoken");
+const { auth } = require("./configs/auth.js");
+
+//Bringing in Express
+const express = require("express");
+const app = express();
+
+//OTHER IMPORTS
+const morgan = require("morgan");
+
+////////////
+//MIDDLEWARE
+////////////
+NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+app.use(morgan("tiny")); //logging
+
+///////////////
+//Routes and Routers
+//////////////
+app.get("/", (req, res) => {
+  res.json({ hello: "Hello World!" });
+});
+
+//These routes are to generate a test JWT and test out your auth function from auth.js
+app.get("/testauth", auth(SECRET), (req, res) => {
+  res.json(req.payload);
+});
+
+app.get("/testjwt", (req, res) => {
+  const token = jwt.sign({ hello: "world" }, SECRET);
+  res.json({ token });
+});
+
+//LISTENER
+app.listen(PORT, () => {
+  console.log(`Your are listening on port ${PORT}`);
+});
